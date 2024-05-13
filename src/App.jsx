9256aba9a973f4ccc26a1c8a5668febc1ab97f7f
Temi <stringsondeck@gmail.com>
2024-05-13 21:03:26 +0100

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import Homepage from "../page/Homepage.jsx";
 
 function App() {
   return (
     <div>
       <Header />
-      <div>body</div>
+      <Homepage />
       <Footer />
     </div>
   );
