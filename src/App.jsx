d610ea95a5bdb459242fccc57a809ef1bc6391db
Temi <stringsondeck@gmail.com>
2024-05-13 20:44:14 +0100

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-import { RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-import { router } from "./Router";
+// Pages
+import Homepage from "./pages/Homepage.jsx";
+import Login from "./pages/Login.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Error404 from "./pages/Error404.jsx";
+import RootLayout from "./layout/RootLayout.jsx";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<RootLayout />}>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="*" element={<Error404 />} />
+    </Route>
+  )
+);
 
 function App() {
   return <RouterProvider router={router} />;
